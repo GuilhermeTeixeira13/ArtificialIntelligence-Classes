5049138829bf3214f585ed0a119d51acd4dd3154
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2022-10-17 12:58:28 +0100

    N-Queens Problem

--- AI/Week2/queens.py
+++ AI/Week2/queens.py
@@ -1,4 +1,6 @@
 from tabulate import tabulate
+from datetime import datetime
+
 
 def pos1_to_pos2(x, n):
     row = x // n
@@ -9,11 +11,12 @@ def pos1_to_pos2(x, n):
 def pos2_to_pos1(x2, n):
     return x2[0] * n + x2[1]
 
+
 def show_board(board, n):
     table = []
     table_line = []
     for pos in range(n*n):
-        table_line.append(board[pos])
+        table_line.append(board[pos]+1)
         if (pos + 1) % n == 0:
             table.append(table_line)
             table_line = []
@@ -21,99 +24,115 @@ def show_board(board, n):
     print(tabulate(table))
 
 
-def diagonalPri(pos2D, n):
-    pos2Dx = pos2D[0]
-    pos2Dy = pos2D[1]
-    diagonal_pri = []
+def negative_diagonal(pos2D, n):
+    nd = []
 
     # up
-    new2Dx = pos2Dx
-    new2Dy = pos2Dy
-    while (new2Dx + 1 < n and new2Dy + 1 < n):
-        diagonal_pri.append([new2Dx + 1, new2Dy + 1])
-        new2Dx += 1
-        new2Dy += 1
+    x = pos2D[0]
+    y = pos2D[1]
+    while x + 1 < n and y + 1 < n:
+        nd.append([x + 1, y + 1])
+        x += 1
+        y += 1
 
     # down
-    new2Dx = pos2Dx
-    new2Dy = pos2Dy
-    while (new2Dx - 1 >= 0 and new2Dy - 1 >= 0):
-        diagonal_pri.append([new2Dx - 1, new2Dy - 1])
-        new2Dx -= 1
-        new2Dy -= 1
+    x = pos2D[0]
+    y = pos2D[1]
+    while x - 1 >= 0 and y - 1 >= 0:
+        nd.append([x - 1, y - 1])
+        x -= 1
+        y -= 1
 
-    return diagonal_pri
+    return nd
 
-def diagonalSec(pos2D, n):
-    pos2Dx = pos2D[0]
-    pos2Dy = pos2D[1]
-    diagonal_sec = []
+
+def positive_diagonal(pos2D, n):
+    pd = []
 
     # up
-    new2Dx = pos2Dx
-    new2Dy = pos2Dy
-    while(new2Dx + 1 < n and new2Dy - 1 >= 0):
-        diagonal_sec.append([new2Dx + 1, new2Dy - 1])
-        new2Dx += 1
-        new2Dy -= 1
+    x = pos2D[0]
+    y = pos2D[1]
+    while x + 1 < n and y - 1 >= 0:
+        pd.append([x + 1, y - 1])
+        x += 1
+        y -= 1
 
     # down
-    new2Dx = pos2Dx
-    new2Dy = pos2Dy
-    while (new2Dx - 1 >= 0 and new2Dy + 1 < n):
-        diagonal_sec.append([new2Dx - 1, new2Dy + 1])
-        new2Dx -= 1
-        new2Dy += 1
+    x = pos2D[0]
+    y = pos2D[1]
+    while x - 1 >= 0 and y + 1 < n:
+        pd.append([x - 1, y + 1])
+        x -= 1
+        y += 1
 
-    return diagonal_sec
+    return pd
+
 
 def diagonals(pos2D, n):
-    return diagonalSec(pos2D, n) + diagonalPri(pos2D, n)
+    return positive_diagonal(pos2D, n) + negative_diagonal(pos2D, n)
 
 
-def is_attacking(pos, board, n):
-    posAnalysed2D = pos1_to_pos2(pos, n)
-    is_attacking = False
+def vertical(pos2D, n):
+    v = []
 
-    for posBoard1D in range(n*n):
-        posBoard2D = pos1_to_pos2(posBoard1D, n);
-        if posBoard2D != posAnalysed2D and board[posBoard1D] == 1:
-            # Row verification
-            if posBoard2D[0] == posAnalysed2D[0]:
-                is_attacking = True
-            # Column verification
-            if posBoard2D[1] == posAnalysed2D[1]:
-                is_attacking = True
+    x_lower = pos2D[0] - 1
+    while x_lower >= 0:
+        v.append([x_lower, pos2D[1]])
+        x_lower -= 1
 
-    for pos2D in diagonals(posAnalysed2D, n):
-        if board[pos2_to_pos1(pos2D, n)] == 1:
-            is_attacking = True
+    x_upper = pos2D[0] + 1
+    while x_upper < n:
+        v.append([x_upper, pos2D[1]])
+        x_upper += 1
 
-    return is_attacking
+    return v
+
+
+def block_arround(pos2D, board, n):
+    to_block = vertical(pos2D, n) + diagonals(pos2D, n)
+    board[pos2_to_pos1(pos2D, n)] = 1
+
+    for item2D in to_block:
+        if board[pos2_to_pos1(item2D, n)] != -1:
+            board[pos2_to_pos1(item2D, n)] = -1
 
 
 def is_final_board(board, n):
-    # The final board is a board where there is no empty spaces in what the queen not attacking other pieces
-    final_board = True
+    # If the number of queens equals n, then the board is final
+    if board.count(1) == n:
+        return True
 
-    for pos in range(n*n):
-        if board[pos] != 1:
-            if not is_attacking(pos, board, n):
-                final_board = False
-
-    return final_board
+    return False
 
 
-def get_successors(board, n):
+def first_row_with_pos_available(board, n):
+    table_line = []
+    line = -1
+    for pos in range(n * n):
+        table_line.append(board[pos])
+        if (pos + 1) % n == 0:
+            line += 1
+            if table_line.count(1) == 0:
+                return line + 1
+            table_line = []
+
+    return -1
+
+
+def get_successors(board, n, row_tested):
     sucs = []
+    row_tested_beginning = n*(row_tested-1)
+    row_tested_end = n * row_tested
 
-    for posBoard1D in range(n*n):
-        if board[posBoard1D] != 1 and not is_attacking(posBoard1D, board, n):
-            nv = board.copy()
-            nv[posBoard1D] = 1
-            sucs.append(nv)
+    if row_tested != -1:
+        for posBoard1D in range(row_tested_beginning, row_tested_end):
+            if board[posBoard1D] == 0:
+                nv = board.copy()
+                block_arround(pos1_to_pos2(posBoard1D, n), nv, n)
+                sucs.append(nv)
 
     return sucs
+
 
 def find_node(tr, id):
     if len(tr) == 0:
@@ -126,8 +145,9 @@ def find_node(tr, id):
             return aux
     return None
 
-def successors(tr, st, n):
-    suc = get_successors(st[0], n)
+
+def successors(tr, st, n, row_tested):
+    suc = get_successors(st[0], n, row_tested)
     for b in suc:
         aux = find_node(tr, b)
         if aux is None:
@@ -151,33 +171,41 @@ def insert_sorted(lst, nv):
     return lst[:index] + [nv] + lst[index:]
 
 
-# depth first
 def search(st, n, type_search):
     # type_search: 1=breadth first; 2=depth first; 3=A*
     done = []
     todo = [st]
+    num_sol = 0
+    sol_leet = []
     while len(todo) > 0:
         cur = todo.pop(0)
         if is_final_board(cur[0], n):
-            return st, cur
+            #  show_board(cur[0], n)
+            num_sol += 1
 
-        cur = successors(st, cur, n)
+        cur = successors(st, cur, n, first_row_with_pos_available(cur[0], n))
         for s in cur[-1]:
             if board_in_list(s[0], done):
                 continue
             if type_search == 1:
-                todo.append(s) # Queue push to end
+                todo.append(s)  # Queue push to end
             elif type_search == 2:
-                todo.insert(0, s) # Stack push to beginning
+                todo.insert(0, s)  # Stack push to beginning
 
         done.insert(-1, cur[0])
-    return st, None
+
+    return num_sol
+
+###################################
 
 
+start_time = datetime.now()
 
-n = 4
+n = 8
 initial_board = [0] * (n * n)
 initial_state = [initial_board, []]
-initial_state, result = search(initial_state, n, 1)
-print(result)
-show_board(result[0], n)
+num_solutions = search(initial_state, n, 2)
+print(num_solutions)
+
+end_time = datetime.now()
+print(end_time-start_time)
