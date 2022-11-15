71fe8ee13fa892db277ebac06a2b3b826123f115
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2022-11-15 15:20:32 +0000

    .

--- AI/TP2/tp2.py
+++ AI/TP2/tp2.py
@@ -5,28 +5,32 @@ import csv
 def create_instances(data, t):
     activities = ["Downstairs", "Jogging", "Sitting", "Standing", "Upstairs", "Walking"]
     user = 1
-    item = ""
+    act = 5
+    item = [1, 5]
+    line_count = 2
     with open(data, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_count = 0
-        for row in csv_reader:
-            if line_count > 0:
-                if line_count == 1:
-                    item = "1/"
-
-                # Muda de user
-                if str(user) == row["user"]:
-                    item = item + (f'{row["x-axis"]},{row["y-axis"]},{row["z-axis"]}/')
-                    line_count += 1
-                else:
-                    user += 1
-                    item = item + str(user) + '/' + (f'{row["x-axis"]},{row["y-axis"]},{row["z-axis"]}/')
-                    line_count += 1
-            else:
+        with open('sorted.csv', 'w', encoding='UTF8') as f:
+            csv_reader = csv.DictReader(csv_file)
+            writer = csv.writer(f)
+            for row in csv_reader:
+                if str(user) != row["user"] or str(activities.index(row["activity"])) != str(act):
+                    if str(user) != row["user"]:
+                        user += 1
+                    if str(activities.index(row["activity"])) != str(act):
+                        act = activities.index(row["activity"])
+                    writer.writerow(item)
+                    item = []
+                    item.append(str(user))
+                    item.append(str(act))
+                item.append(row["x-axis"])
+                item.append(row["y-axis"])
+                item.append(row["z-axis"])
                 line_count += 1
-            if line_count == t*20:
-                break
-        print(item)
 
-#str(activities.index(row["activity"]))
-create_instances("time_series_data_human_activities.csv", 5500)
+                # Tirar próximas 2 linhas caso seja para guardar a info toda
+                if line_count == t * 20:
+                    break
+            writer.writerow(item)
+
+
+create_instances("time_series_data_human_activities.csv", 1)
