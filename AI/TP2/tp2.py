fcf8b37b6aa5eb0a3ba5433cc4964fdcb4405a8e
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-12-03 23:09:01 +0000

    tp2

--- AI/TP2/tp2.py
+++ AI/TP2/tp2.py
@@ -4,42 +4,41 @@ import csv
 
 def create_instances(data, t):
     activities = ["Downstairs", "Jogging", "Sitting", "Standing", "Upstairs", "Walking"]
-    user = 1
-    act = 5
     item = [1, 5]
     with open(data, mode='r') as data:
         with open('data_organized.csv', 'w', encoding='UTF8') as data_organized:
             data_reader = csv.DictReader(data)
+            data_reader_list = list(data_reader)
             writter_data_organized = csv.writer(data_organized)
 
-            total_lines = sum(1 for row in data_reader)
-            begin_in_line = 2
+            total_lines = sum(1 for item in data_reader_list)
+            begin_in_line = 0
             line_limit = t * 20
 
-            while(line_limit <= total_lines):
-                line_count = begin_in_line
-                for i, row in enumerate(data_reader):
-                    if i == line_count and line_count != line_limit:
-                        if str(user) != row["user"] or str(activities.index(row["activity"])) != str(act):
-                            if str(user) != row["user"]:
-                                user += 1
-                            if str(activities.index(row["activity"])) != str(act):
-                                act = activities.index(row["activity"])
-                            writter_data_organized.writerow(item)
-                            item = []
-                            item.append(str(user))
-                            item.append(str(act))
+            user = data_reader_list[0]["user"]
+            act = activities.index(data_reader_list[0]["activity"])
 
-                        item.append(row["x-axis"])
-                        item.append(row["y-axis"])
-                        item.append(row["z-axis"])
-                        line_count += 1
+            print("LIMIT:"+str(line_limit)+" // TOTAL:"+str(total_lines))
 
-                        #Tirar próximas 2 linhas caso seja para guardar a info toda
-                        #if line_count == line_limit:
-                        #    break
-                writter_data_organized.writerow(item)
+            while line_limit <= total_lines:
+                print("BEGIN:"+str(begin_in_line)+" -> END:"+str(line_limit))
+                for i in range(begin_in_line, line_limit):
+                    if str(user) != data_reader_list[i]["user"] or str(activities.index(data_reader_list[i]["activity"])) != str(act):
+                        if str(user) != data_reader_list[i]["user"]:
+                            user = data_reader_list[i]["user"]
+                        if str(activities.index(data_reader_list[i]["activity"])) != str(act):
+                            act = activities.index(data_reader_list[i]["activity"])
+
+                        writter_data_organized.writerow(item)
+                        item = []
+                        item.append(str(user))
+                        item.append(str(act))
+
+                    item.append(data_reader_list[i]["x-axis"])
+                    item.append(data_reader_list[i]["y-axis"])
+                    item.append(data_reader_list[i]["z-axis"])
+
                 begin_in_line += 1
                 line_limit += 1
 
-create_instances("time_series_data_human_activities.csv", 1)
+create_instances("time_series_data_human_activities.csv", 53680)
